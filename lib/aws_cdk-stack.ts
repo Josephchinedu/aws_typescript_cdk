4a1ddd5e98f742df2038ca9e28558b5084241fe1
Joseph Chinedu <joseph.chinedu@globalaccelerex.com>
2022-04-07 15:55:52 +0100

    first aws cdk

--- lib/aws_cdk-stack.ts
+++ lib/aws_cdk-stack.ts
@@ -1,10 +1,16 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsCdkStack extends cdk.Stack {
+  constructor(scope:cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new s3.Bucket(this, 'MyBucket', {
+      bucketName: 'joebucket-codeengine'
+    })   
 
     // The code that defines your stack goes here
 
